d4e125a9431f1ac2e773c4437af6af092b3ed77d
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-03-10 13:27:25 -0300

    Fet:Query - Exibe o histórico de reprodução para cada pessoa usuária

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+SELECT us.usuario, count(hp.FK_id_musica) as qtde_musicas_ouvidas, ROUND((sum(mu.duracao)/60),2) as total_minutos
+FROM SpotifyClone.usuarios as us
+INNER JOIN SpotifyClone.historico_de_reproducoes as hp
+ON us.id_usuario = hp.FK_id_usuario 
+INNER JOIN SpotifyClone.musicas as mu
+ON mu.id_musica = hp.FK_id_musica
+group by us.usuario
