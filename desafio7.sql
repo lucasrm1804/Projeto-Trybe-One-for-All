5e5bd4bc9672adea28b4ec9d0874d5a5c8d12701
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-03-14 11:24:51 -0300

    FIX: Alteraçoes para passar no requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,5 @@ inner join SpotifyClone.albuns as al
 on al.FK_id_artista = ar.id_artista
 inner join SpotifyClone.seguindo_artistas as sa
 on  ar.id_artista = sa.FK_id_artista
-group by album
-order by  seguidores desc, artista, album
+group by ar.artista, al.album
+order by seguidores desc, artista, al.album
