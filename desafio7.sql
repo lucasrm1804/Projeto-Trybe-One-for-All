69f2c5d48a7aa2863ad119c5072c10825cea7c5e
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-03-14 10:29:28 -0300

    FEAT: Query: Exibe uma relação de todos os álbuns produzidos por cada artista

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+select ar.artista, al.album, count(sa.FK_id_artista) as seguidores from SpotifyClone.artistas as ar 
+inner join SpotifyClone.albuns as al
+on al.FK_id_artista = ar.id_artista
+inner join SpotifyClone.seguindo_artistas as sa
+on  ar.id_artista = sa.FK_id_artista
+group by album
+order by  seguidores desc, artista, album
